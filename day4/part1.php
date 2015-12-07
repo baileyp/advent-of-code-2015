53728fc0f7f1525266638cd73b5b7111317a0a1c
Peter Bailey <peter.bailey@jwt.com>
2015-12-07 13:26:06 -0600

    Day 4 complete!

---
+++ day4/part1.php
@@ -0,0 +1,12 @@
+<?php
+
+define('KEY', 'ckczppom');
+
+$i = 0;
+do {
+    $i += 1;
+    $hash = md5(KEY . $i);
+}
+while (strpos($hash, '00000') !== 0);
+
+echo 'Lowest positive number: ', $i, PHP_EOL;
